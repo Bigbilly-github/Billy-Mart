090691e95d2bc2f94013a0b77fd9b75057358afa
Bigbilly-github <abiolatwitter97@gmail.com>
2025-05-15 13:56:11 +0100

    getting products data from dummy json api

--- src/contexts/propscontext.jsx
+++ src/contexts/propscontext.jsx
@@ -6,6 +6,25 @@ const valueContext = createContext();
 
 
 function ContextProvider ({ children }){
+
+
+
+    useEffect(()=>{
+        const getApiDetail = async ()=>{
+            try{
+                const response = await fetch('https://dummyjson.com/products?limit=0');
+                const responseData = await response.json();
+                console.log(responseData);
+
+            }
+            catch(err) {
+                console.log( err)
+            }
+           
+        }
+        getApiDetail();
+
+    },[])
     return(
         <>
          <valueContext.Provider value={{}}>
