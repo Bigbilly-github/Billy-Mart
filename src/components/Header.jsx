4345c8a92fec4ba401e56d2229bdf4db5561dc1d
Bigbilly-github <abiolatwitter97@gmail.com>
2025-05-19 22:49:10 +0100

    creating and styling the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,81 @@
-import React from 'react'
+import React from "react";
+import search from "../assets/svg/header/search.svg";
+import heart from "../assets/svg/header/heart1.svg";
+import cart from "../assets/svg/header/cart.svg";
 
 const Header = () => {
   return (
-        <>
-        <header className='h-[100px] fixed right-0 left-0 bg-[red] flex justify-center items-center'>
+    <>
+      <header className="h-[150px] fixed right-0 left-0  flex justify-center items-center">
+        <div className="w-[90%] h-[88px]  rounded-[40px] shadow-lg flex justify-between items-center  px-[30px]">
+          <p className="font-[poppins] text-[24px] font-bold">Billy-Mart</p>
 
-        </header>
-        </>
-  )
-}
+          <nav className="w-[367px] h-[38px]  font-[Poppins] flex justify-between ">
+            <a
+              href=""
+              className="text-[16px] hover:underline hover:text-slate-500 "
+            >
+              Home
+            </a>
+            <a
+              href=""
+              className="text-[16px] hover:underline hover:text-slate-500"
+            >
+              Shop
+            </a>
+            <a
+              href=""
+              className="text-[16px] hover:underline hover:text-slate-500 "
+            >
+              About
+            </a>
+            <a
+              href=""
+              className="text-[16px] hover:underline hover:text-slate-500 "
+            >
+              Contact Us
+            </a>
+          </nav>
 
-export default Header
+          <div className="w-[376px] h-[38px]  justify-between flex  items-center">
+            <div className="flex flex-1 h-[40px] mr-[20px] px-[10px] py-[10px] pl-[20px] bg-[#00000020] items-center rounded-[20px]">
+              <input
+                type="text"
+                className="flex-1 outline-0"
+                placeholder="Search products"
+              />
+              <img
+                src={search}
+                alt="search icon"
+                className="w-[24px] h-[24px]"
+              />
+            </div>
+            <div className="flex justify-between w-[70px] ">
+              <div className="relative ">
+                <img
+                  src={heart}
+                  alt="heart icon"
+                  className="w-[24px] h-[24px]"
+                />
+                <p className="absolute -top-[19px] -right-[5px] font-[poppins] font-medium">
+                  2
+                </p>
+              </div>
+              <div className="relative">
+                    <img src={cart} alt="cart icon" className="w-[24px] h-[24px]" />
+                    <p className="absolute -top-[15px] -right-[5px] font-[poppins] font-medium">
+                        2
+                     </p>
+
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
