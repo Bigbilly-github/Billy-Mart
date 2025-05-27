f293175a0f91b42341b020dc659b19a88ead9635
Bigbilly-github <abiolatwitter97@gmail.com>
2025-05-27 08:56:20 +0100

    changes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,7 +9,7 @@ import Ad from '../components/ad'
 import NewArrival from '../components/newarrival'
 import CustomerService from '../components/customerservices'
 import Footer from '../components/footer'
-import Hero from '../components/hero'
+import Hero from '../components/Hero'
 
 
 
