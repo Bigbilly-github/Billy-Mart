d0d5bf339a9d21b3b868d1b811fc3e3308c02367
Bigbilly-github <abiolatwitter97@gmail.com>
2025-06-05 16:49:55 +0100

    added the delete icon to the cart items enabling users to remove items from the carts

--- src/components/cartdisplay.jsx
+++ src/components/cartdisplay.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import beauty from "../assets/img/category/beauty.jpeg"
 import { useValueContext } from '../contexts/propscontext'
+import deleteicon from "../assets/svg/cart/icon-cancel.svg"
 
 function CartDisplay() {
   const { cart, products,HandleQuantity ,setCart} = useValueContext();
@@ -31,12 +32,19 @@ function CartDisplay() {
    localStorage.setItem('cart', JSON.stringify(updatedCart));
 }
 
+function DeleteCartItem (id) {
+  const newcart = cart.filter(item=> item.id !== id);
+     setCart(newcart);
+   localStorage.setItem('cart', JSON.stringify(newcart));
+
+}
+
 
   return (
     <section className="w-full min-h-[600px] py-[50px] flex justify-center">
       <div className="w-[90%] flex flex-col gap-[40px]">
        
-        <div className="flex items-center h-[72px] shadow-md rounded-[4px] bg-gray-100">
+        <div className="flex w-[90%] sm:w-[95%] items-center h-[72px] shadow-md rounded-[4px] bg-gray-100">
           <p className="w-[25%] text-center font-medium text-[14px] sm:text-[16px]">Product</p>
           <p className="w-[25%] text-center font-medium text-[14px] sm:text-[16px]">Price</p>
           <p className="w-[25%] text-center font-medium text-[14px] sm:text-[16px]">Quantity</p>
@@ -45,39 +53,44 @@ function CartDisplay() {
 
        
         {cartProducts.map((item, index) => (
-          <div key={index} className="flex items-center h-[82px] sm:pl-0 pl-[10px]  shadow-md rounded-[4px] justify-around">
-            <div className="flex w-[25%] justify-center items-center sm:gap-[20px] gap-[10px] text-center">
-              <img src={item.thumbnail} alt={item.title} className="w-[50px] h-[50px] hidden sm:block " />
-              <p className="md:text-[14px] text-[12px]">{item.title}</p>
+        <div className="flex gap-[10px] sm:gap-[20px]">
+            <div key={index} className="flex w-[90%] sm:w-[95%] items-center h-[82px] sm:pl-0 pl-[10px]  shadow-md rounded-[4px] justify-around">
+                  <div className="flex w-[25%] justify-center items-center sm:gap-[20px] gap-[10px] text-center">
+                    <img src={item.thumbnail} alt={item.title} className="w-[50px] h-[50px] hidden sm:block " />
+                    <p className="md:text-[14px] text-[12px]">{item.title}</p>
+                  </div>
+
+                  <div className="w-[25%] text-center">
+                    <p className="md:text-[14px] text-[12px]">${item.price}</p>
+                  </div>
+
+                  <div className="w-[25%] text-center">
+                    <select
+                      name={item.id}
+                      value={item.quantity}
+                      onChange={(e)=>HandleQuantity2(item.id,e)}
+                      
+                      className="px-[12px] outline-0 md:text-[18px] sm:text-[16px] py-[6px] text-[12px] rounded-[5px] bg-slate-200"
+                    >
+                      {[1, 2, 3, 4, 5].map(q => (
+                        <option key={q} value={q}>{q}</option>
+                      ))}
+                    </select>
+                  </div>
+
+                  <div className="w-[25%] text-center">
+                    <p className="md:text-[14px] text-[12px]">
+                ${(Number(item.price) * Number(item.quantity)).toFixed(2)}
+
+                    </p>
+                  </div>
+
             </div>
 
-            <div className="w-[25%] text-center">
-              <p className="md:text-[14px] text-[12px]">${item.price}</p>
-            </div>
+            <button onClick={()=>DeleteCartItem(item.id)}><img src={deleteicon} alt="delete icon" /></button>
 
-            <div className="w-[25%] text-center">
-              <select
-                name={item.id}
-                value={item.quantity}
-                onChange={(e)=>HandleQuantity2(item.id,e)}
-                
-                className="px-[12px] outline-0 md:text-[18px] sm:text-[16px] py-[6px] text-[12px] rounded-[5px] bg-slate-200"
-              >
-                {[1, 2, 3, 4, 5].map(q => (
-                  <option key={q} value={q}>{q}</option>
-                ))}
-              </select>
-            </div>
-
-            <div className="w-[25%] text-center">
-              <p className="md:text-[14px] text-[12px]">
-           ${(Number(item.price) * Number(item.quantity)).toFixed(2)}
-
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
+        </div>))}
+    </div>
     </section>
   );
 }
