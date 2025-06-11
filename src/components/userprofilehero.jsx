31e8145bb4d31223314c09a83ede8b3de109d8fc
Bigbilly-github <abiolatwitter97@gmail.com>
2025-06-11 15:11:42 +0100

    changes

--- src/components/userprofilehero.jsx
+++ src/components/userprofilehero.jsx
@@ -1,54 +1,64 @@
 
 import groceries from "../assets/img/category/groceries.jpg"
+import { useValueContext } from '../contexts/propscontext'
 
 function UserProfileHero (){
+     const {  placedorders,setPlacedOrders,products} = useValueContext();
+
+     function GetTitle (id){
+        const match = products.find(product=> product.id===id);
+        return match.title;
+
+     }
 
     return(
         <>
-        <section className="w-full flex  mt-[50px] h-[600px] justify-center">
+        <section className="w-full flex  mt-[50px] h-auto justify-center">
             <div className="w-[90%] flex ">
                 <div className="w-[20%] border-r border-slate-200">
                     <h1 className="text-[24px] cursor-pointer hover:underline transition-all duration-150">
                         My Orders
                     </h1>
 
-                    <h1 className="text-[24px] mt-[20px] cursor-pointer hover:underline transition-all duration-150">
-                        My Wishlist
-                    </h1>
+                   
 
                 </div>
-                <div className="w-[80%] flex flex-col ">
-                    <div>
-                        <h1 className="text-[36px] pl-[40px] mb-[30px]">
-                            Order 1
+                <div className="w-[80%] flex flex-col  ">
+                  {placedorders.map((orders,index)=> (
+                     <div key={index + 1} className="mb-[30px]">
+                        <h1 className="text-[36px] text-slate-700 pl-[40px] font-semibold mb-[30px]">
+                            Order {index + 1}
                         </h1>
-                        <div className="shadow-md px-[30px] pb-[30px]">
-                                <div className="flex items-center gap-[10px] ">
+                      {orders.map((order,index) =>(
+                         <div key={index} className="shadow-md px-[30px] mb-[30px] pb-[30px]">
+                                <div className="flex items-center gap-[15px] ">
                                     <img src={groceries} alt=""  className="w-[40px] h-[40px] rounded-[50%]"/>
-                                    <p>
-                                        Title
+                                    <p className="font-medium text-[18px]">
+                                       {GetTitle(order.id)}
                                     </p>
                                 </div>
                             
                                     <div className="flex pl-[50px] justify-between items-center mt-[20px]">
-                                        <p>
-                                            Quantity: $450
+                                        <p className="text-slate-600">
+                                            Quantity: {order.quantity}
                                         </p>
-                                        <p>
-                                            Price: $450
+                                       <p className="text-slate-600">
+                                            Price: ${order.price}
                                         </p>
-                                        <p>
-                                            Delivery: $450
+                                        <p className="text-slate-600">
+                                            Delivery: ${order.delivery}
                                         </p>
-                                        <p>
-                                            Total: $450
+                                      <p className="text-slate-600">
+                                            Total: ${(Number(order.price) * Number(order.quantity)).toFixed(2)}
                                         </p>
                                     </div>
                       
 
                         </div>
+                  ))}
 
                     </div>
+                ))}
 
 
 
