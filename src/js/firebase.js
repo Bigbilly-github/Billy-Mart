4eb8ca26e9e6ee22a28d6255feb30e09648d689e
Bigbilly-github <abiolatwitter97@gmail.com>
2025-06-14 09:24:30 +0100

    hiding my secret keys

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCS3ZMvMybdpwJhtfnektSeyuF6qN_vaf4",
-  authDomain: "billymart-e337a.firebaseapp.com",
-  projectId: "billymart-e337a",
- storageBucket: "billymart-e337a.appspot.com",
-  messagingSenderId: "1019835237065",
-  appId: "1:1019835237065:web:42e6ea5a28d2f301330318",
-  measurementId: "G-RFXV27NE4V"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 
