34425acc165b14681b8fa39f1b342292bc6798a2
Bigbilly-github <abiolatwitter97@gmail.com>
2025-06-19 13:48:18 +0100

    added inetraction to the nav links, added underline styling to each page link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,8 @@ import carticon from "../assets/svg/header/cart.svg";
 import user from "../assets/svg/header/user.svg";
 import { Link } from "react-router-dom";
 import { useValueContext } from "../contexts/propscontext";
+import { NavLink } from "react-router-dom";
+
 
 const Header = () => {
    const { cart,wishlist} = useValueContext();
@@ -14,31 +16,49 @@ const Header = () => {
         <div className="w-[90%] bg-white sm:h-[88px] h-[60px]   rounded-[40px] shadow-lg flex justify-between items-center  px-[30px]">
            <Link to="/homepage"><p className="font-[poppins] lg:text-[24px]  sm:text-[18px] font-bold">Billy-Mart</p></Link>
 
-          <nav className="lg:w-[450px] md:w-[300px]  w-[280px] h-[38px] hidden items-center  font-[Poppins] md:flex justify-between ">
-            <Link
-              to="/homepage"
-              className="md:text-[14px] font-medium   lg:text-[16px] hover:underline hover:text-slate-500 "
-            >
-              Home
-            </Link>
-            <Link
-              to="/shop"
-              className="md:text-[14px] font-medium  lg:text-[16px]hover:underline hover:text-slate-500"
-            >
-              Shop
-            </Link>
-            <Link
-              to="/aboutus"
-              className="md:text-[14px] lg:text-[16px] font-medium  hover:underline hover:text-slate-500 "
-            >
-              About
-            </Link>
-            <Link
-              to="/contactus"
-              className="md:text-[14px] font-medium   lg:text-[16px] hover:underline hover:text-slate-500 "
-            >
-              Contact Us
-            </Link>
+          <nav className="lg:w-[450px] md:w-[300px]   w-[280px] h-[38px] hidden items-center  font-[Poppins] md:flex justify-between ">
+            <NavLink
+                to="/homepage"
+                className={({ isActive }) =>
+                  `md:text-[14px] font-medium lg:text-[16px]  hover:text-slate-500 ${
+                    isActive ? "underline text-slate-700" : ""
+                  }`
+                }
+              >
+                Home
+              </NavLink>
+
+           <NavLink
+                  to="/shop"
+                  className={({ isActive }) =>
+                    `md:text-[14px] font-medium lg:text-[16px] hover:underline hover:text-slate-500 ${
+                      isActive ? "underline text-slate-700" : ""
+                    }`
+                  }
+                >
+                  Shop
+          </NavLink>
+
+            <NavLink
+                  to="/aboutus"
+                  className={({ isActive }) =>
+                    `md:text-[14px] font-medium lg:text-[16px] hover:underline hover:text-slate-500 ${
+                      isActive ? "underline text-slate-700" : ""
+                    }`
+                  }
+                >
+                  About Us
+          </NavLink>
+          <NavLink
+                  to="/contactus"
+                  className={({ isActive }) =>
+                    `md:text-[14px] font-medium lg:text-[16px] hover:underline hover:text-slate-500 ${
+                      isActive ? "underline text-slate-700" : ""
+                    }`
+                  }
+                >
+                 Contact Us
+          </NavLink>
           </nav>
 
           <div className="   h-[38px]  justify-end flex  items-center">
@@ -50,7 +70,7 @@ const Header = () => {
                   alt="heart icon"
                   className="lg:w-[24px] lg:h-[24px] w-[20px] h-[20px]"
                 /></Link>
-                <p className="absolute -top-[19px] sm:text-[16px] lg:text-[18px] text-[14px] -right-[5px] font-[poppins] font-medium">
+                <p className="absolute -top-[19px]  sm:text-[16px] lg:text-[18px] text-[14px] -right-[5px] font-[poppins] font-medium">
                   {wishlist.length}
                 </p>
               </div>
